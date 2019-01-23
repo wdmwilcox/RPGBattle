e6ca9526f320f57f663db83c1b6f39da2db9639d
wdmwilcox <46979186+wdmwilcox@users.noreply.github.com>
2019-01-23 17:02:32 -0600

    Add files via upload

---
+++ RPGBattle.py
@@ -0,0 +1,44 @@
+# namespace
+import stats
+
+# globals
+
+# classes
+
+class Character(object):
+	
+	__init__(self):
+		pass
+
+		
+class Player(Character):
+	
+	__init__(self, name):
+		self.name = name
+		pass
+		
+
+class Enemy(Character):
+
+	__init__(self, name):
+		self.name = name
+		pass
+
+class Engine(object):
+	
+	__init__(self):
+		pass
+		
+	def new_battle():
+		pass
+	
+	def play():
+		pass
+	
+
+def main():
+	game = Engine()
+	
+	
+main()
+
