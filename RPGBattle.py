5a734c13aef5e00fb9bf329acce5bc43ec003c38
wdmwilcox <46979186+wdmwilcox@users.noreply.github.com>
2019-01-24 17:38:04 -0600

    Add files via upload

--- RPGBattle.py
+++ RPGBattle.py
@@ -1,5 +1,6 @@
 # namespace
-import stats
+import random
+import time
 
 # globals
 
@@ -7,38 +8,61 @@ import stats
 
 class Character(object):
 	
-	__init__(self):
-		pass
-
+	def __init__(self):
+		self.stats = {'health' : 100.0,
+		'attack power' : 1.0,
+		'defense' : 1.0,
+		}
+		
 		
 class Player(Character):
 	
-	__init__(self, name):
+	def __init__(self, name):
 		self.name = name
-		pass
 		
 
 class Enemy(Character):
 
-	__init__(self, name):
+	def __init__(self, name):
 		self.name = name
 		pass
 
+
 class Engine(object):
 	
-	__init__(self):
+	def __init__(self):
 		pass
-		
-	def new_battle():
+
+	def new_battle(self):
 		pass
-	
-	def play():
-		pass
-	
+
+	def get_help(self):
+		print(f'This feature is not implemented.')
+		self.play()
+
+	def quit_game(self):
+		print(f'This feature is implemented.')
+		exit(0)
+
+	def play(self):
+		print('Welcome to RPG Battle!')
+		print("Type 'help' to read the rules; type 'start' to begin a new game, or type 'quit' to exit.")
+		play_input = input(' > ')
+		if play_input.lower() == 'help':
+			self.get_help()
+		elif play_input.lower() == 'start':
+			print(f'This feature is not implemented.')
+			self.play()
+		elif play_input.lower() == 'quit':
+			self.quit_game()
+		else:
+			print(f'The feature {help_or_start} is not implemented.')
+			self.play()
+
 
 def main():
 	game = Engine()
-	
+	game.play()
 	
 main()
 
