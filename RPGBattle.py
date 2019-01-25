ee2c89fdca3b55cc1b8af1a1fdd9d0f5e34f3100
wdmwilcox <46979186+wdmwilcox@users.noreply.github.com>
2019-01-24 19:04:19 -0600

    Add files via upload

--- RPGBattle.py
+++ RPGBattle.py
@@ -23,17 +23,34 @@ class Player(Character):
 
 class Enemy(Character):
 
-	def __init__(self, name):
-		self.name = name
-		pass
+	def __init__(self):
+		self.dialogue = {'meet' : [],
+						 'hit' : [],
+						 'miss' : [],
+						 'defeat' : [],
+						 'victory' : [],
+						}
 
+
+class Gnome(Enemy):
+
+	def __init__(self, level):
+		self.name = 'Gnome'
+		self.dialogue = {'hit' : ['Ouch',
+							      'Darnit',
+								  'Argh',
+								  ],
+						 'meet' : ['I\'m a guh-nome!',
+								   'You\'re \'bout to get gnomed!',
+								   'Watch your knees!',
+								   'Fresh out the garden!',
+								   ],
+						 }
+								   
 
 class Engine(object):
 	
 	def __init__(self):
-		pass
-
-	def new_battle(self):
 		pass
 
 	def get_help(self):
@@ -44,6 +61,20 @@ class Engine(object):
 		print(f'This feature is implemented.')
 		exit(0)
 
+	def get_player_name(self):
+		print(f'You wake up with amnesia.  What is your name?')
+		player_name = input(' > ')
+		print(f'''Ah yes, {player_name}.  You feel a bruise on your head and see a simple wooden club at your feet.
+A quick check of your pockets reveal that your gold is missing.  You have been robbed!  You aren't going to take this
+lying down; you reach down to grab the club and find some monster footprints leading away.  It's time to get your money back!''')
+		return player_name
+
+	def new_battle(self):
+		
+		monster = Enemy(player_level)
+	
+
+
 	def play(self):
 		print('Welcome to RPG Battle!')
 		print("Type 'help' to read the rules; type 'start' to begin a new game, or type 'quit' to exit.")
@@ -51,8 +82,8 @@ class Engine(object):
 		if play_input.lower() == 'help':
 			self.get_help()
 		elif play_input.lower() == 'start':
-			print(f'This feature is not implemented.')
-			self.play()
+			self.player_name = self.get_player_name()
+			print(f'Hello, {self.player_name}.  This feature is not implemented.')
 		elif play_input.lower() == 'quit':
 			self.quit_game()
 		else:
