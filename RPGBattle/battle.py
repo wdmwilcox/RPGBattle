7574bdf3b3eecf38cc1db6bd74f3619072fd72ab
wdmwilcox <46979186+wdmwilcox@users.noreply.github.com>
2019-03-09 12:32:22 -0600

    Add files via upload

--- RPGBattle/battle.py
+++ RPGBattle/battle.py
@@ -1,28 +1,25 @@
 class Battle(object):
-	
+
 
 	def __init__(self, player_level):
 		self.player_level = player_level
 		self.battle_over = False
 		self.enemies = []
-	
-	def generate_enemies(self, player_level):
+
+	def generate_enemies(self):
 		pass
-		
+
 	def display_information(self):
 		pass
-		
+
 	def display_enemy_dialogue(self):
 		pass
-		
-	def get_choices(self):
-		pass
-		
+
 	def calculate_outcome(self):
 		pass
-		
+
 	def get_enemy_turn(self):
 		pass
-		
+
 	def end_battle(self):
 		pass
